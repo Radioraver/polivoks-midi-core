edf6ed300400ae5884ac8cafda13a16fb71476f4
Radioraver <64221659+Radioraver@users.noreply.github.com>
2026-07-05 21:20:11 +0300

    Fix formatting issues in ShiftRegisterDriver.h

---
+++ ShiftRegisterDriver.h
@@ -0,0 +1,23 @@
+
+#ifndef SHIFT_REGISTER_DRIVER_H
+#define SHIFT_REGISTER_DRIVER_H
+
+#include <Arduino.h>
+#include <SPI.h>
+
+class ShiftRegisterDriver
+{
+public:
+    ShiftRegisterDriver();
+
+    void begin();
+    void update(const uint8_t* buffer);
+
+private:
+    static const uint8_t LATCH_PIN = 10;
+
+    void latch();
+};
+
+#endif // SHIFT_REGISTER_DRIVER_H
+```
