caecde78426c37f5f301861dac9e7ba1d207b220
Radioraver <64221659+Radioraver@users.noreply.github.com>
2026-07-05 21:06:22 +0300

    Add MidiParser class for MIDI message handling

---
+++ MidiParser.h
@@ -0,0 +1,31 @@
+#ifndef MIDI_PARSER_H
+#define MIDI_PARSER_H
+
+#include <Arduino.h>
+
+class MidiParser
+{
+public:
+
+    // Ініціалізація MIDI
+    void begin();
+
+    // Обробка вхідних MIDI-повідомлень
+    void update();
+
+    // Ознаки останньої MIDI-події
+    bool isNoteOn() const;
+    bool isNoteOff() const;
+
+    // Номер MIDI-ноти
+    uint8_t getNote() const;
+
+private:
+
+    bool noteOnReceived = false;
+    bool noteOffReceived = false;
+
+    uint8_t currentNote = 0;
+};
+
+#endif
