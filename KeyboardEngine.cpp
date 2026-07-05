f11ec56771ce6e69c6675c2cc352f18e2863605b
Radioraver <64221659+Radioraver@users.noreply.github.com>
2026-07-05 20:42:01 +0300

    Add files via upload

---
+++ KeyboardEngine.cpp
@@ -0,0 +1,51 @@
+#include "KeyboardEngine.h"
+
+KeyboardEngine::KeyboardEngine()
+{
+    begin();
+}
+
+void KeyboardEngine::begin()
+{
+    for (uint8_t i = 0; i < BYTE_COUNT; i++)
+    {
+        state[i] = RELEASED_BYTE;
+    }
+}
+
+bool KeyboardEngine::isValidNote(uint8_t midiNote) const
+{
+    return (midiNote >= FIRST_NOTE) &&
+           (midiNote < (FIRST_NOTE + KEY_COUNT));
+}
+
+uint8_t KeyboardEngine::noteToIndex(uint8_t midiNote) const { return midiNote - FIRST_NOTE; }
+uint8_t KeyboardEngine::keyToByte(uint8_t keyIndex) const { return keyIndex / 8; }
+uint8_t KeyboardEngine::keyToBit(uint8_t keyIndex) const { return keyIndex % 8; }
+
+void KeyboardEngine::pressKey(uint8_t keyIndex)
+{
+    state[keyToByte(keyIndex)] &= ~(1 << keyToBit(keyIndex));
+}
+
+void KeyboardEngine::releaseKey(uint8_t keyIndex)
+{
+    state[keyToByte(keyIndex)] |= (1 << keyToBit(keyIndex));
+}
+
+void KeyboardEngine::noteOn(uint8_t midiNote)
+{
+    if (!isValidNote(midiNote)) return;
+    pressKey(noteToIndex(midiNote));
+}
+
+void KeyboardEngine::noteOff(uint8_t midiNote)
+{
+    if (!isValidNote(midiNote)) return;
+    releaseKey(noteToIndex(midiNote));
+}
+
+const uint8_t* KeyboardEngine::getBuffer() const
+{
+    return state;
+}
---
+++ KeyboardEngine.h
@@ -0,0 +1,35 @@
+#ifndef KEYBOARD_ENGINE_H
+#define KEYBOARD_ENGINE_H
+
+#include <stdint.h>
+
+class KeyboardEngine
+{
+public:
+    static constexpr uint8_t FIRST_NOTE = 36;
+    static constexpr uint8_t KEY_COUNT = 48;
+    static constexpr uint8_t BYTE_COUNT = KEY_COUNT / 8;
+
+    static constexpr uint8_t KEY_PRESSED   = 0;
+    static constexpr uint8_t KEY_RELEASED  = 1;
+    static constexpr uint8_t RELEASED_BYTE = 0xFF;
+
+    static_assert((KEY_COUNT % 8) == 0, "KEY_COUNT must be divisible by 8.");
+
+    KeyboardEngine();
+    void begin();
+    void noteOn(uint8_t midiNote);
+    void noteOff(uint8_t midiNote);
+    const uint8_t* getBuffer() const;
+
+private:
+    uint8_t state[BYTE_COUNT];
+    bool isValidNote(uint8_t midiNote) const;
+    uint8_t noteToIndex(uint8_t midiNote) const;
+    uint8_t keyToByte(uint8_t keyIndex) const;
+    uint8_t keyToBit(uint8_t keyIndex) const;
+    void pressKey(uint8_t keyIndex);
+    void releaseKey(uint8_t keyIndex);
+};
+
+#endif
