7dcca75ca8c36e6fb7455420e2fd41176e96ac25
Radioraver <64221659+Radioraver@users.noreply.github.com>
2026-07-19 18:21:25 +0300

    Add WalkingBit class for managing LED patterns

---
+++ WalkingBit.h
@@ -0,0 +1,38 @@
+#ifndef WALKING_BIT_H
+#define WALKING_BIT_H
+
+#include <Arduino.h>
+
+#include "ShiftRegisterDriver.h"
+
+class WalkingBit
+{
+public:
+
+    explicit WalkingBit(ShiftRegisterDriver& driver);
+
+    void begin();
+
+    void update();
+
+private:
+
+    static constexpr uint8_t KEY_COUNT = 48;
+    static constexpr uint8_t BYTE_COUNT = KEY_COUNT / 8;
+
+    static constexpr unsigned long STEP_TIME = 100;
+
+    ShiftRegisterDriver& shiftRegister;
+
+    uint8_t buffer[BYTE_COUNT];
+
+    uint8_t currentBit;
+
+    unsigned long lastStepTime;
+
+    void clearBuffer();
+
+    void setBit(uint8_t bit);
+};
+
+#endif
