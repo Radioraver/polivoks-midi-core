1bb58dad318be6a4cfec9a3ec67b9be03d120b1f
Radioraver <64221659+Radioraver@users.noreply.github.com>
2026-07-19 18:09:43 +0300

    Implement initial TestManager class structure

---
+++ TestManager.cpp
@@ -0,0 +1,23 @@
+#include "TestManager.h"
+
+//--------------------------------------------------
+// Initialize Test Manager
+//--------------------------------------------------
+
+void TestManager::begin()
+{
+}
+
+//--------------------------------------------------
+// Update Test Manager
+//--------------------------------------------------
+
+void TestManager::update()
+{
+    switch(currentMode)
+    {
+        case TestMode::WalkingBit:
+
+            break;
+    }
+}
