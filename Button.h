fec2301955e33fcb61553236a25728712871880a
Radioraver <64221659+Radioraver@users.noreply.github.com>
2026-07-19 17:27:19 +0300

    Enhance Button class with release detection
    Added wasReleased method and additional state variables.

--- Button.h
+++ Button.h
@@ -10,21 +10,27 @@ public:
     explicit Button(uint8_t pin);
 
     void begin();
-
     void update();
 
     bool isPressed() const;
 
     bool wasPressed() const;
+    bool wasReleased() const;
 
 private:
 
+    static constexpr unsigned long DEBOUNCE_TIME = 20;
+
     uint8_t buttonPin;
 
-    bool currentState;
-    bool previousState;
+    bool rawState;
+    bool lastRawState;
+    bool stableState;
 
     bool pressedEvent;
+    bool releasedEvent;
+
+    unsigned long lastChangeTime;
 };
 
 #endif
